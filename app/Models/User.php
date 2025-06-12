96ffc82282952bf5309ac4597e6fb1a819a1e0d9
Mikhail Eismont <supermen696@yandex.ru>
2025-06-12 20:48:21 +0400

    fix user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,7 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    const UPDATED_AT = null;
     /**
      * The attributes that are mass assignable.
      *
